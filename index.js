3171bfbfa1028eac12c676492bc2225219b612c2
lmcredstonedingz <landree.allen04@gmail.com>
2016-12-08 10:48:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
   var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     width = canvas.width,
-    height = canvas.height
-  ctx.fillStyle = "rgb(255,0,0)";
+    height = canvas.height;
+  
+  ctx.fillStyle = "#111111";
   ctx.rect(0,0,width,height);
   ctx.fill();
 })()
