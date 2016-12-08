3e815af4d730d38133adc79138ca285870ce528b
lmcredstonedingz <landree.allen04@gmail.com>
2016-12-08 15:24:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
   
   function drawChar(font,char,x,y) {
     var c = char.charCodeAt(0);
-    ctx.drawImage(font,((c-(Math.floor(c/16)*16))*8)+1,(Math.floor(c/16)*12)+1,8,12,x,y,8,12);
+    ctx.drawImage(font,(c-(Math.floor(c/16)*16))*8,Math.floor(c/16)*12,8,12,x,y,8,12);
   }
   
   ctx.imageSmoothingEnabled = false;
