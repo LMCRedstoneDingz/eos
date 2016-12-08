7982c39a3779b5debc37b244b9ec39f3378fa3ce
lmcredstonedingz <landree.allen04@gmail.com>
2016-12-08 03:55:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+(function() {
+  var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    width = canvas.width,
+    height = canvas.height
+  ctx.fillStyle = "rgb(255,0,0)";
+  ctx.rect(0,0,width,height);
+  ctx.fill();
+})()
